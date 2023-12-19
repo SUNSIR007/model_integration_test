f43cf5a96187879bce7ef422e93ea991378e40ce
王世玉 <wangshiyuo@foxmail.com>
2023-12-19 14:32:51 +0800

    fix:告警查询接口合并

--- apps/routers/v1/alarm.py
+++ apps/routers/v1/alarm.py
@@ -42,6 +42,39 @@ async def create_alarm(
     )
 
 
+@router.delete(
+    "/alarms/{alarm_id}",
+    description="删除告警记录",
+)
+def delete_alarm_record(
+        alarm_id: int,
+        current_user: Account = Depends(get_current_user),
+        db_session: Session = Depends(get_db_session),
+):
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=403,
+            detail="Access denied",
+        )
+
+    # 查询要删除的告警记录
+    alarm = db_session.query(Alarm).get(alarm_id)
+    if not alarm:
+        raise HTTPException(
+            status_code=404,
+            detail="Alarm record not found",
+        )
+
+    # 删除告警记录
+    db_session.delete(alarm)
+    db_session.commit()
+
+    return GeneralResponse(
+        code=200,
+        meg="Alarm record deleted successfully!"
+    )
+
+
 @router.put(
     "/alarms",
     description="更新告警记录",
@@ -97,6 +130,7 @@ def get_alarm_record(
     if end_time:
         query = query.filter(Alarm.alarmTime <= end_time)
 
+    total_count = query.count()
     alarm = query.order_by(desc(Alarm.alarmTime)).offset((page_no - 1) * page_size).limit(page_size).all()
 
     if not alarm:
@@ -104,76 +138,11 @@ def get_alarm_record(
 
     return GeneralResponse(
         code=200,
-        data=alarm
-    )
-
-
-@router.delete(
-    "/alarms/{alarm_id}",
-    description="删除告警记录",
-)
-def delete_alarm_record(
-        alarm_id: int,
-        current_user: Account = Depends(get_current_user),
-        db_session: Session = Depends(get_db_session),
-):
-    if not current_user.is_active:
-        raise HTTPException(
-            status_code=403,
-            detail="Access denied",
-        )
-
-    # 查询要删除的告警记录
-    alarm = db_session.query(Alarm).get(alarm_id)
-    if not alarm:
-        raise HTTPException(
-            status_code=404,
-            detail="Alarm record not found",
-        )
-
-    # 删除告警记录
-    db_session.delete(alarm)
-    db_session.commit()
-
-    return GeneralResponse(
-        code=200,
-        meg="Alarm record deleted successfully!"
-    )
-
-
-@router.get(
-    "/alarms/page",
-    description="告警记录分页",
-)
-def get_alarm_records_page(
-        current_user: Account = Depends(get_current_user),
-        db_session: Session = Depends(get_db_session),
-        page_no: int = Query(..., gt=0),
-        page_size: int = Query(..., gt=0, le=100),
-        filter_params: AlarmFilterParams = Depends(),
-):
-    if not current_user.is_active:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Access denied",
-        )
-
-    conditions = []
-    for key, value in filter_params.dict(exclude_unset=True).items():
-        if value is not None:
-            conditions.append(getattr(Alarm, key).ilike(f"%{value}%"))
-
-    query = db_session.query(Alarm).filter(or_(*conditions))
-    total_count = query.count()
-    alarm_records = query.order_by(desc(Alarm.alarmTime)).offset((page_no - 1) * page_size).limit(page_size).all()
-
-    return {
-        "code": 200,
-        "data": {
-            "list": alarm_records,
+        data={
+            "list": alarm,
             "total": total_count,
         }
-    }
+    )
 
 
 @router.get(
