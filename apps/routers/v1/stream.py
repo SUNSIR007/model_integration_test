2f078756922117b59213300b96a240019b5d7726
王世玉 <wangshiyuo@foxmail.com>
2024-01-02 09:56:04 +0800

    feat:流媒体推理

--- apps/routers/v1/stream.py
+++ apps/routers/v1/stream.py
@@ -1,13 +1,13 @@
 import cv2
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from ultralytics import YOLO
 
 from apps.database import get_db_session
-from apps.models import Algorithm, Camera
+from apps.models import Algorithm, Camera, Account
 from apps.config import settings
-
+from apps.routers.v1.auth import get_current_user
 
 router = APIRouter(tags=["流媒体推理"])
 
@@ -37,9 +37,16 @@ def generate_frames(video_url, model):
 def video_feed(
         camera_id: int,
         algorithm_id: int,
-        session: Session = Depends(get_db_session)
+        session: Session = Depends(get_db_session),
+        current_user: Account = Depends(get_current_user)
 ):
-    model_name = session.query(Algorithm).filter(Algorithm.id==algorithm_id).first().modelName
-    video_url = session.query(Camera).filter(Camera.camera_id==camera_id).first().video_url
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=403,
+            detail="Access denied",
+        )
+
+    model_name = session.query(Algorithm).filter(Algorithm.id == algorithm_id).first().modelName
+    video_url = session.query(Camera).filter(Camera.camera_id == camera_id).first().video_url
     model = YOLO(f'apps/detection/weights/{model_name}')
     return StreamingResponse(generate_frames(video_url, model), media_type='multipart/x-mixed-replace; boundary=frame')
